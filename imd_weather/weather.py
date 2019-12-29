be89a7295494fe69d9713451d46f0d87bed17ed6
Anjan Roy <anjanroy@yandex.com>
2019-12-29 20:24:43 +0530

    refactored code; now code is cleaner and concise

--- imd_weather/weather.py
+++ imd_weather/weather.py
@@ -1,103 +1,77 @@
 #!/usr/bin/python3
 
-try:
-    from urllib.parse import urlencode
-    from bs4 import BeautifulSoup
-    from requests import get
-    from records import __validate_city_id__, fetch_city_name_id
-except ImportError as e:
-    print('[!]Module Unavailable : {}'.format(str(e)))
-    exit(1)
+from __future__ import annotations
+from bs4 import BeautifulSoup, Tag
+from requests import get
+from urllib.parse import urljoin
+from typing import Dict, Any, List, Tuple
 
 
-def __extract_7_days_forecast__(data):
+def _forecast(data: Tag) -> List[Tuple[str, str, str, str, str]]:
     '''
-        next 7 days forecast data gets extracted from web page by this function.
+        Extracts next 7 day's forecast for a certain city
     '''
-    parsed = []
-    try:
-        data = data.findAll('tr')[2:]
-        for i in data:
-            tmp = []
-            for j in i.findAll('td'):
-                if(j.findChild('img')):
-                    tmp.append(j.find('img').get('src'))
-                else:
-                    tmp.append(j.getText().strip())
-            parsed.append(tmp)
-    except Exception as e:
-        parsed = [str(e)]
-    return parsed
+    return list(map(lambda e: tuple(map(lambda e:
+                                        urljoin('http://city.imd.gov.in/citywx/',
+                                                e.find('img').get('src')) if e.findChild('img')
+                                        else e.getText().strip(),
+                                        e.findAll('td'))), data.findAll('tr')[2:]))
 
 
-def __extract_past_24_hours_weather_data__(data):
+def _history(data: Tag) -> Dict[str, str]:
     '''
-        past 24 hours weather data extracted from web page, here.
+        Extracts past 24 hour's weather for a specified city
     '''
+    data = data.findAll('td')[1:]
+    gap = 2
+    return list(map(lambda e: (e[0].getText().strip(), e[1].getText().strip()),
+                    map(lambda e: data[e:e+gap], range(0, len(data), gap))))
+
+
+def _parse(content: str) -> Dict[str, Any]:
     parsed = {}
-    try:
-        data = data.findAll('td')[1:]
-        gap = 2
-        conflict = 'Departure from Normal(oC)'
-        conflict_count = 0
-        for i in range(0, len(data), gap):
-            tmp = data[i:i+gap]
-            if(tmp[0].getText().strip() == conflict):
-                parsed.update({tmp[0].getText().strip()+'{}'.format(conflict_count): tmp[1].getText().strip()})
-                conflict_count += 1
-            else:
-                parsed.update({tmp[0].getText().strip(): tmp[1].getText().strip()})
-    except Exception as e:
-        parsed = {'error': str(e)}
+    handle = BeautifulSoup(content, features='lxml')
+    tables = handle.findAll('table')
+    if tables[0].find('td').findChild('img'):
+        parsed.update(
+            {
+                'location': urljoin('http://city.imd.gov.in/citywx/', tables[0].find('td').find('img').get('src'))
+            })
+    if tables[3].find('td').findChild('img'):
+        parsed.update(
+            {
+                'graph': urljoin('http://city.imd.gov.in/citywx/', tables[3].find('td').find('img').get('src'))
+            })
+    parsed.update(
+        {
+            'history': _history(tables[1]),
+            'forecast': _forecast(tables[2])
+        })
     return parsed
 
 
-def __parse_content__(content):
-    '''
-        Parse content using BeautifulSoup, returns python dict.
-    '''
-    parsed = {}
-    try:
-        handle = BeautifulSoup(content, features='lxml')
-        tables = handle.findAll('table')
-        if(tables[0].find('td').findChild('img')):
-            parsed.update({'location_on_map_image': tables[0].find('td').find('img').get('src')})
-        parsed.update({'past_24_hours_weather': __extract_past_24_hours_weather_data__(tables[1])})
-        parsed.update({'7_days_forecast': __extract_7_days_forecast__(tables[2])})
-        if(tables[3].find('td').findChild('img')):
-            parsed.update({'weather_graph': tables[3].find('td').find('img').get('src')})
-    except Exception as e:
-        parsed = {'error': str(e)}
-    return parsed
-
-
-def __get_content__(base_url):
+def _get(url: str) -> Dict[str, Any]:
     '''
         Sends get request at imd website and returns back parsed response.
     '''
     try:
-        resp = get(base_url, headers={'Accept': 'text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8', 'Accept-Language': 'en-US,en;q=0.5', 'Connection': 'keep-alive', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:60.0) Gecko/20100101 Firefox/62.0'})
-        if(not resp.ok):
-            raise Exception('got {} from server'.format(resp.status_code))
-        return __parse_content__(resp.content)
+        resp = get(url, headers={
+            'Accept': 'text/html,application/xhtml+xml,application/xml,application/json;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Connection': 'keep-alive',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:60.0) Gecko/20100101 Firefox/71.0'
+        })
+        if not resp.ok:
+            raise Exception('Got {} from server'.format(resp.status_code))
+        return _parse(resp.content)
     except Exception as e:
         return {'error': str(e)}
 
 
-def fetch(city_id, override_check=False, db_name='imd_city_db', base_url='http://city.imd.gov.in/citywx/city_weather.php'):
-    '''
-        Fetches weather info of certain city, designated by city_id, from imd website and returns in parsed form.
-        set `override_check` to True, if you have'nt yet stored city name & id, into local database.
-        city_id is mandatory argument.
-    '''
-    if(__validate_city_id__(city_id)):
-        if(override_check):
-            return {city_id: __get_content__(base_url+'?{}'.format(urlencode([('id', city_id)])))}
-        if(fetch_city_name_id(city_id=city_id, db_name=db_name).get(city_id, [])):
-            return {city_id: __get_content__(base_url+'?{}'.format(urlencode([('id', city_id)])))}
-    return {'error': 'city id not validated'}
+def fetch(url: str) -> Dict[str, Any]:
+    return _get(url)
 
 
 if __name__ == '__main__':
     print('[!]This module is designed to be used as a backend handler')
-    exit(0)     
+    exit(0)
